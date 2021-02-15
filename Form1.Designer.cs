8d0d6a2e30a15b12ef4b2bc16b327cb319f870e0
klukovka <58118015+klukovka@users.noreply.github.com>
2021-02-15 16:45:23 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Add_binary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.First = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Second = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(119, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Сложение двоичных чисел";
+            // 
+            // First
+            // 
+            this.First.Location = new System.Drawing.Point(106, 95);
+            this.First.Name = "First";
+            this.First.Size = new System.Drawing.Size(433, 26);
+            this.First.TabIndex = 1;
+            this.First.TextChanged += new System.EventHandler(this.GetRes);
+            this.First.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(305, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "+";
+            // 
+            // Second
+            // 
+            this.Second.Location = new System.Drawing.Point(106, 176);
+            this.Second.Name = "Second";
+            this.Second.Size = new System.Drawing.Size(433, 26);
+            this.Second.TabIndex = 3;
+            this.Second.TextChanged += new System.EventHandler(this.GetRes);
+            this.Second.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(305, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 39);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "=";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Result.Location = new System.Drawing.Point(56, 268);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(533, 39);
+            this.Result.TabIndex = 5;
+            this.Result.Text = "                                           ";
+            this.Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(646, 358);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Second);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.First);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Add";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox First;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Second;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Add_binary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        string AddBinaryNumbers(string first="0", string second="0")
+        {
+
+            string result = "";
+            int len = first.Length > second.Length ? first.Length : second.Length;
+            first = first.PadLeft(len, '0');
+            second = second.PadLeft(len, '0');
+            first = Reverse(first);
+            second = Reverse(second);
+            string temp = "0";
+            for (int i = 0; i < len; i++)
+            {
+                if (temp == "0")
+                {
+                    if (first[i]=='0' && second[i]=='0')
+                    {
+                        result += "0";
+                        temp = "0";
+                    }
+                    else if (first[i]=='1' && second[i] == '1')
+                    {
+                        result += "0";
+                        temp = "1";
+                    }
+                    else
+                    {
+                        result += "1";
+                        temp = "0";
+                    }
+                }
+                else
+                {
+                    if (first[i] == '0' && second[i] == '0')
+                    {
+                        result += "1";
+                        temp = "0";
+                    }
+                    else if (first[i] == '1' && second[i] == '1')
+                    {
+                        result += "1";
+                        temp = "1";
+                    }
+                    else
+                    {
+                        result += "0";
+                        temp = "1";
+                    }
+                }
+            }
+            if (temp == "1")
+                result += temp;
+            result = Reverse(result);
+           
+            return result;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            if ((e.KeyChar <= 47 || e.KeyChar >= 50) && e.KeyChar != 8 && e.KeyChar!=127)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void GetRes(object sender, EventArgs e)
+        {
+            Result.Text = AddBinaryNumbers(First.Text, Second.Text);
+        }
+    }
+}
